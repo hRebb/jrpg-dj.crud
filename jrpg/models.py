5d46e2cbf5f11e20626e8e5526fcfb6895b6d41d
hamza_rebb <hamza.rebb@gmail.com>
2023-06-28 21:04:21 +0200

    create model and make migration

--- jrpg/models.py
+++ jrpg/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=100)
+    console = models.CharField(max_length=100)
+    adaptation= models.CharField(max_length=100)
