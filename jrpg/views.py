0641c2bf9b09eb4fe6d09088bb189441d76d8663
hamza_rebb <hamza.rebb@gmail.com>
2023-06-28 21:10:02 +0200

    add views

--- jrpg/views.py
+++ jrpg/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from models import Game
 
 # Create your views here.
+
+
+def game_list(request):
+    games = Game.objects.all()
+    return render(
+        request,
+        'jrpg/game_list.html',
+        {
+            'games': games
+        }
+    )
+
+
+def add_game(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        publisher = request.POST['publisher']
+        console = request.POST['console']
+        adaptation = request.POST['adaptation']
+        game = Game(
+            name=name,
+            publisher=publisher,
+            console=console,
+            adaptation=adaptation
+        )
+        game.save()
+        return redirect('jrpg/game_list.html')
+    return render(
+        request,
+        'jrpg/add_game.html'
+    )
